63e880b99371369bddf824aa28f455cd579b5350
krantheman <akash.akasht.tom@gmail.com>
2021-06-30 11:24:12 +0530

    Navbar blueprint

--- tracr/frontend/src/components/App.js
+++ tracr/frontend/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
 
+
 const App = () => {
     return (
         <div>
--- tracr/frontend/src/components/HomePage.js
+++ tracr/frontend/src/components/HomePage.js
@@ -1,18 +1,43 @@
 import React from "react";
+import { Navbar, Nav, Col, Button, Container, Row } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 
 const HomePage = (props) => {
     return (
-        <div> 
-            <h1>Home Page</h1>
-            <p>This is the {props.name} page</p>
+        <div>
+            <NavBar />
         </div>
-        // <Router>
-        //     <Switch>
-        //         <Route exact path='/'><p>This is the home page</p></Route>
-        //         <Route path='/other' component={OtherPage}></Route>
-        //     </Switch>
-        // </Router>
+    );
+}
+
+const NavBar = (props) => {
+    return (
+        <div> 
+            <Container>
+                <Navbar className='nav-color' fixed='top'>
+                    <Col md={{ span:2, offset: 1}}>
+                        <Row>
+                            <Navbar.Brand href='/'>
+                                <img src='../../static/images/logo.png' alt='tracr'/>
+                            </Navbar.Brand>
+                            <Nav>
+                                <Nav.Link>Home</Nav.Link>
+                            </Nav>
+                        </Row>
+                    </Col>
+                    <Col md={{ span:2, offset: 6}}>
+                        <Nav className='justify-content-end'>
+                            <Nav.Item>
+                                <Nav.Link>Login</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Button variant='outline-warning'>Get Started</Button>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
+                </Navbar>
+            </Container>
+        </div>
     );
 }
 
