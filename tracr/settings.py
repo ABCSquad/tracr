e5a905795161ec2db202bafc10151f8a12efd5a0
Bryan Ronad <58883939+BryanRonad@users.noreply.github.com>
2023-01-21 17:15:21 +0530

    Update settings.py

--- tracr/settings.py
+++ tracr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q*9eiu#s+zet$0t9^@(9km7ny-a*y#*k9zam*!17sa))ahc@nf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["scribblesolve.onrender.com"]
 
 
 # Application definition
